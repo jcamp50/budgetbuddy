b31966ea5ab22412ce9975f47d6ed7bc9971e60e
jcamp50 <7jacampbell@gmail.com>
2024-02-04 15:23:22 -0500

    backend setup

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -1,10 +1,10 @@
-const IncomeSchema = require("../models/IncomeModel")
+const ExpenseSchema = require("../models/expenseModel")
 
 
-exports.addIncome = async (req, res) => {
+exports.addExpense = async (req, res) => {
   const {title, amount, category, description, date} = req.body
 
-  const income = new IncomeSchema({
+  const expense = new ExpenseSchema({
     title,
     amount,
     category,
@@ -20,30 +20,30 @@ exports.addIncome = async (req, res) => {
       if(amount <= 0 || !amount === 'number'){
         return res.status(400).json({error: 'Amount must be a number and greater than 0'})
       }
-      await income.save()
-      res.status(200).json({message: 'Income added successfully'})
+      await expense.save()
+      res.status(200).json({message: 'Expense added successfully'})
   } catch (error) {
     res.status(500).json({message: 'Server error', error: error.message})
   }
 
-  console.log(income)
+  console.log(expense)
 }
 
-exports.getIncomes = async (req, res) => {
+exports.getExpenses = async (req, res) => {
   try {
-    const incomes  = await IncomeSchema.find().sort({createdAt: -1})
-    res.status(200).json(incomes)
+    const expenses  = await ExpenseSchema.find().sort({createdAt: -1})
+    res.status(200).json(expenses)
   } catch (error) {
     res.status(500).json({message: 'Server Error'})
   }
 }
 
-exports.deleteIncome = async (req, res) => {
+exports.deleteExpense = async (req, res) => {
   const {id} = req.params
   
-  IncomeSchema.findByIdAndDelete(id)
-    .then((income) => {
-      res.status(200).json({message: 'Income deleted successfully'})
+  ExpenseSchema.findByIdAndDelete(id)
+    .then((expense) => {
+      res.status(200).json({message: 'Expense deleted successfully'})
       
     })
     .catch((err) => {
