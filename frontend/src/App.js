7b1a4d4f1a4b3bc5be509e853ad53b75bfbb0d99
jcamp50 <7jacampbell@gmail.com>
2024-02-04 15:55:11 -0500

    rem useless files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,4 @@
-import React, {useState, useMemo} from 'react'
-import styled from "styled-components";
-import bg from './img/bg.png'
-import {MainLayout} from './styles/Layouts'
-import Orb from './Components/Orb/Orb'
-import Navigation from './Components/Navigation/Navigation'
-import Dashboard from './Components/Dashboard/Dashboard';
-import Income from './Components/Income/Income'
-import Expenses from './Components/Expenses/Expenses';
-import { useGlobalContext } from './context/globalContext';
+
 
 function App() {
   
