0a46f7abcec7151c85da3ab4322f8bfa15a3941a
Jordan Campbell <7jacampbell@gmail.com>
2024-02-05 17:55:38 -0500

    orb animation finished

--- frontend/src/Components/Orb/Orb.js
+++ frontend/src/Components/Orb/Orb.js
@@ -1,36 +1,37 @@
-import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { useWindowSize } from '../../utils/useWindowSize';
 
 function Orb() {
+  const { width, height } = useWindowSize();
+
+  console.log(width, height);
 
   const moveOrb = keyframes`
     0%{
       transform: translate(0, 0);
     }
     50%{
-      transform: translate(400px, 500px);
+      transform: translate(${width / 1.2}px, ${height / 2}px);
     }
     100%{
       transform: translate(0, 0);
     }
   `;
 
-
   const OrbStyled = styled.div`
-      width: 70vh;
-      height: 70vh;
-      position: absolute;
-      border-radius: 50%;
-      margin-left: -37vh;
-      marign-top: -37vh;
-      background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
-      filter: blur(400px);
-      animation: ${moveOrb} 5s alternate linear infinite;
-    `;
+    width: 70vh;
+    height: 70vh;
+    position: absolute;
+    border-radius: 50%;
+    margin-left: -37vh;
+    marign-top: -37vh;
+    background: linear-gradient(180deg, #f56692 0%, #f2994a 100%);
+    filter: blur(400px);
+    animation: ${moveOrb} 10s alternate linear infinite;
+  `;
 
-  return (
-    <OrbStyled></OrbStyled>
-  )
+  return <OrbStyled></OrbStyled>;
 }
 
-export default Orb
+export default Orb;
